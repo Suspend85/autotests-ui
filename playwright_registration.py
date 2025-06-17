1e0dc9a04f7a055ddfec99f39a68c357a498ca85
Dmitrii Krop <melomanko@gmail.com>
2025-06-17 20:19:36 +0700

    Введение в Playwright. Практика с Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -23,7 +23,6 @@ with sync_playwright() as playwright:
     login_button.click()
 
     context.storage_state(path='browser-state.json')
-
     page.wait_for_timeout(3000)
 
 
@@ -34,3 +33,4 @@ with sync_playwright() as playwright:
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
 
     page.wait_for_timeout(5000)
+
