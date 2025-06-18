49179c5daa03d4240eaa4e1ec70dae97ed1c1b1e
Dmitrii Krop <melomanko@gmail.com>
2025-06-18 17:12:35 +0700

    Pytest skip, skipif, xfail

---
+++ tests/test_pytest_xfail.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+@pytest.mark.xfail(reason="Баг с 1 == 2")
+def test_with_bug():
+    assert 1 == 2
+
+
+@pytest.mark.xfail(reason="Баг уже исправлен")
+def test_without_bug():
+    ...
+
+@pytest.mark.parametrize(reason="Баг уже исправлен")
+def test_without_bug():
+    ...
