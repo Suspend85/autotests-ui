80aefb5a019c985b0e17a81ba65aaae8771b3331
Dmitrii Krop <melomanko@gmail.com>
2025-07-22 05:41:58 +0700

    Практикуемся в реализации PageComponent. Практикуемся в написании компонентов с использованием паттерна PageComponent

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,6 @@
 from playwright.sync_api import Page, expect
+
+from components.authentication.login_form_component import LoginFormComponent
 from pages.base_page import BasePage
 
 
@@ -6,18 +8,13 @@ class LoginPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.email_input = page.get_by_test_id('login-form-email-input').locator('input')
-        self.password_input = page.get_by_test_id('login-form-password-input').locator('input')
+        self.login_form = LoginFormComponent(page)
         self.login_button = page.get_by_test_id('login-page-login-button')
         self.registration_link = page.get_by_test_id('login-page-registration-link')
         self.wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
 
     def fill_login_form(self, email: str, password: str):
-        self.email_input.fill(email)
-        expect(self.email_input).to_have_value(email)
-
-        self.password_input.fill(password)
-        expect(self.password_input).to_have_value(password)
+        self.login_form.fill(email, password)
 
     def click_login_button(self):
         self.login_button.click()
@@ -25,6 +22,7 @@ class LoginPage(BasePage):
     def click_registration_link(self):
         self.registration_link.click()
 
-    def check_visible_wrong_email_or_password_alert(self):
+    def check_visible_wrong_email_or_password_alert(self, email: str, password: str):
+        self.login_form.check_visible(email, password)
         expect(self.wrong_email_or_password_alert).to_be_visible()
         expect(self.wrong_email_or_password_alert).to_have_text("Wrong email or password")
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,4 +1,3 @@
-from playwright.sync_api import Page
 import pytest
 from pages.courses_list_page import CoursesListPage
 from pages.create_course_page import CreateCoursePage
@@ -20,21 +19,21 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
 @pytest.mark.regression
 def test_create_course(courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
     create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
-    create_course_page.check_visible_create_course_title()
-    create_course_page.check_disabled_create_course_button()
+    create_course_page.check_visible_create_course_title_and_button()
     create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
+
     create_course_page.check_visible_create_course_form('', '', '','0', '0')
-    create_course_page.check_visible_exercises_title()
-    create_course_page.check_visible_create_exercise_button()
+    create_course_page.fill_create_course_form('Playwright', '3h40m', 'playwright','130', '30')
+
+    create_course_page.check_visible_exercise_title_and_button()
     create_course_page.check_visible_exercises_empty_view()
 
     create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.jpg')
     create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
-    create_course_page.fill_create_course_form('Playwright', '2 weeks', 'Playwright', '100', '10')
     create_course_page.click_create_course_button()
 
     courses_list_page.toolbar_view.check_visible()
-    courses_list_page.course_view.check_visible(index=0, title='Playwright', max_score='100', min_score='10', estimated_time='2 weeks')
+    courses_list_page.course_view.check_visible(index=0, title='Playwright', max_score='130', min_score='30', estimated_time='3h40m')
 
 
 
