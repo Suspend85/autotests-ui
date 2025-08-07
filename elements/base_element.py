b47935b2b1966d9bda5ff06eb1a867e0cc85a400
Dmitrii Krop <melomanko@gmail.com>
2025-08-07 19:49:56 +0700

    Практические применение PageFactory в автотестах

--- elements/base_element.py
+++ elements/base_element.py
@@ -7,21 +7,21 @@ class BaseElement:
         self.name = name
         self.locator = locator
 
-    def get_locator(self, **kwargs) -> Locator:
+    def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         locator = self.locator.format(**kwargs)  # create-course-exercise-1000-box-toolbar-subtitle-text
-        return self.page.get_by_test_id(locator)
+        return self.page.get_by_test_id(locator).nth(nth)
         # return self.page.locator(locator)  # как вариант, если на странице нет test_id
 
-    def click(self, **kwargs):
-        locator = self.get_locator(**kwargs)
+    def click(self, nth: int = 0, **kwargs):
+        locator = self.get_locator(nth, **kwargs)
         locator.click()
 
-    def check_visible(self, **kwargs):
-        locator = self.get_locator(**kwargs)
+    def check_visible(self, nth: int = 0, **kwargs):
+        locator = self.get_locator(nth, **kwargs)
         expect(locator).to_be_visible()
 
-    def check_have_text(self, text: str, **kwargs):
-        locator = self.get_locator(**kwargs)
+    def check_have_text(self, text: str, nth: int = 0, **kwargs):
+        locator = self.get_locator(nth, **kwargs)
         expect(locator).to_have_text(text)
 
 
